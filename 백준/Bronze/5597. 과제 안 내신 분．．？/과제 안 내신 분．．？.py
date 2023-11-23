9dc5048a476622c972d4c965af73376addd0bc08
yongha <145304155+yonghaa@users.noreply.github.com>
2023-11-23 21:33:07 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.py
@@ -1,8 +1,5 @@
-students = [i for i in range(1,31)]
-
-for _ in range(28):
-    applied = int(input())
-    students.remove(applied) #소거
-
-print(min(students))
-print(max(students))
+a = list(range(1,31))
+for i in range(28):
+    a.remove(int(input()))
+for i in a:
+    print(i)
