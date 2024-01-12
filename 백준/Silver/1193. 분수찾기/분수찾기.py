8491a6645d24650dec61875bf1f56c55434668d6
yongha <145304155+yonghaa@users.noreply.github.com>
2024-01-12 18:00:16 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,17 @@
+num = int(input())
+line = 1
+
+while num > line:
+    num -= line
+    line += 1
+    
+# 짝수일경우
+if line % 2 == 0:
+    a = num
+    b = line - num + 1
+# 홀수일경우
+elif line % 2 == 1:
+    a = line - num + 1
+    b = num
+
+print(f'{a}/{b}')
